4bec7ffd79017e75f0a5165d6b37dd75ccc1643d
zione <346691836@qq.com>
2024-08-09 23:27:04 +0800

    chore: init

---
+++ constants/index.ts
@@ -0,0 +1,2 @@
+export const appName = '推箱子'
+export const appDescription = '推箱子-小游戏-TDD'
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'home',
+})
+
+</script>
+
+<template>
+  <div>
+    <Suspense>
+      <div>
+        index
+      </div>
+      <template #fallback>
+        <div italic op50>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+  </div>
+</template>
