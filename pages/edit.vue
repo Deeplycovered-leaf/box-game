649c908016d1bee390e40d9f80502ce03fe2bb14
zione <346691836@qq.com>
2024-08-17 18:12:10 +0800

    feat: init map edit

---
+++ pages/edit.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <div flex>
+      <div class="w-2/3 bg-red">
+        edit zone
+      </div>
+      <div>data show zone</div>
+    </div>
+    <div>element select zone</div>
+  </div>
+</template>
+
+<style scoped>
+</style>
